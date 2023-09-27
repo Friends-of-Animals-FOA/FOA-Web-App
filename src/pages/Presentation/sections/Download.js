8c4e41a10dc6acfe4ae328127d487708b36af114
Nikhil Babhulkar <nikhilbabhulkar2019@gmail.com>
2023-09-28 00:03:43 +0530

    changes done in home page

--- src/pages/Presentation/sections/Download.js
+++ src/pages/Presentation/sections/Download.js
@@ -50,25 +50,26 @@ function Download() {
         <Container sx={{ position: "relative", zIndex: 2, py: 12 }}>
           <Grid container item xs={12} md={7} justifyContent="center" mx="auto" textAlign="center">
             <MKTypography variant="h3" color="white">
-              Do you love this awesome
+              Where Compassion Meets Action
             </MKTypography>
-            <MKTypography variant="h3" color="white" mb={1}>
+            {/* <MKTypography variant="h3" color="white" mb={1}>
               UI Kit for ReactJS &amp; MUI?
-            </MKTypography>
+            </MKTypography> */}
             <MKTypography variant="body2" color="white" mb={6}>
-              Cause if you do, it can be yours for FREE. Hit the button below to navigate to
-              Creative Tim where you can find the Design System in HTML. Start a new project or give
-              an old Bootstrap project a new look!
+              At Friends Of Animals, we are on a mission to foster a world where love for animals
+              transforms into meaningful actions. Join us in our journey to protect and care for our
+              furry, feathered, and finned friends. Together, we can make a world of difference, one
+              paw at a time.
             </MKTypography>
             <MKButton
               variant="gradient"
               color="info"
               size="large"
               component="a"
-              href="https://www.creative-tim.com/product/material-kit-react"
+              href=""
               sx={{ mb: 2 }}
             >
-              Download Now
+              Join Our Compassionate Community Today!
             </MKButton>
           </Grid>
         </Container>
